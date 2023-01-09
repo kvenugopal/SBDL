16bfff1477021df3ba3ab966d8cbe4356c79f41d
kvenugopal <kvenugopal@costar.com>
2023-01-08 21:26:15 -0500

    first commit

---
+++ lib/logger.py
@@ -0,0 +1,24 @@
+class Log4j(object):
+    def __init__(self, spark):
+
+        conf = spark.sparkContext.getConf()
+        app_id = conf.get('spark.app.id')
+        app_name = conf.get('spark.app.name')
+
+        log4j = spark._jvm.org.apache.log4j
+        message_prefix = '<' + app_name + ' ' + app_id + '>'
+        self.logger = log4j.LogManager.getLogger(message_prefix)
+
+    def warn(self, message):
+        self.logger.warn(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+
